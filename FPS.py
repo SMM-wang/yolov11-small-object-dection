b95e107371b78e97dbd0dd364349b36e8f75bb0f
wang <1836486705@qq.com>
2026-05-07 13:23:46 +0800

    FPS,onnx

---
+++ FPS.py
@@ -0,0 +1,37 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+import torch
+import time
+import numpy as np
+from ultralytics import YOLO
+
+# 加载模型
+# model = YOLO(r"runs/detect/visdrone2019结果/IOU/SNAIOU/weights/best.pt")
+model = YOLO(r"runs/detect/best.onnx")
+# model = YOLO(r"runs/detect/visdrone2019结果/IOU/SNAIOU/weights/best.pt")
+imgsz = 640
+
+if __name__ == '__main__':
+    # ===================== 官方val测速（最终正确结果，保留）=====================
+    speed_results = model.val(
+        data="my_VisDrone.yaml",
+        imgsz=640,
+        device="0",
+        batch=1,
+        cache=False,
+        plots=False,
+        save=False,
+        verbose=False
+    )
+    pre = speed_results.speed['preprocess']
+    infer = speed_results.speed['inference']
+    post = speed_results.speed['postprocess']
+    total_ms = pre + infer + post
+    fps = 1000 / total_ms
+    
+    print("="*50)
+    print(f"✅ 【官方标准】真实部署FPS: {fps:.2f}")
+    print(f"单帧延迟: {total_ms:.2f}ms (预处理{pre:.1f}+推理{infer:.1f}+后处理{post:.1f})")
+    print("="*50)
+
+   
--- demo.py
+++ demo.py
@@ -1,15 +1,33 @@
-import torch
+# import torch
+# from ultralytics import YOLO
+
+# yolo = YOLO("runs/detect/best.pt")
+# model = yolo.model
+
+# for name, m in model.named_modules():
+#     if m.__class__.__name__ == "SACSP":
+#         print(f"\n=== {name} (SACSP) ===")
+
+#         for bname, bm in m.named_modules():
+#             if isinstance(bm, torch.nn.Conv2d):
+#                 print(f"  {bname}: in={bm.in_channels} out={bm.out_channels} groups={bm.groups}")
+
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 from ultralytics import YOLO
 
-yolo = YOLO("runs/detect/visdrone2019结果/ALL/weights/best.pt")
-model = yolo.model
+# ===================== 配置你的模型路径 =====================
+# 未剪枝模型
+model = YOLO(r"runs/detect/best.onnx")
+# 剪枝模型（需要导出就替换这个路径）
+# model = YOLO(r"runs/detect/best_taylor_pruned_r0.50.pt")
 
-for name, m in model.named_modules():
-    if m.__class__.__name__ == "C2PSA":
-        print(f"\n=== {name} (C2PSA) ===")
-        print(f"  self.c = {m.c}")
-        print(f"  cv1.conv: in={m.cv1.conv.in_channels} out={m.cv1.conv.out_channels}")
-        print(f"  cv2.conv: in={m.cv2.conv.in_channels} out={m.cv2.conv.out_channels}")
-        for bname, bm in m.named_modules():
-            if isinstance(bm, torch.nn.Conv2d):
-                print(f"  {bname}: in={bm.in_channels} out={bm.out_channels} groups={bm.groups}")
+# 导出 TensorRT 引擎（GPU 极致加速）
+model.export(
+    format="engine",  # 导出为 TensorRT
+    imgsz=640,
+    device=0,
+    half=True,  # 开启 FP16 半精度，速度翻倍
+    simplify=True,  # 简化模型，消除通道不对齐问题
+    verbose=True       # 打印日志，能看到进度
+)
